4511038969d998ff4b344cf39b08d4474c2ce082
kali <102199929+SturdyKnight@users.noreply.github.com>
2024-12-10 20:28:55 +0530

    major changes

--- assets/js/team.js
+++ assets/js/team.js
@@ -9,36 +9,6 @@ const teamMembers = [
             twitter: "https://twitter.com/example",
             instagram: "https://www.instagram.com/example"
         }
-    },
-    {
-        name: "Amanda Brown",
-        role: "SECURITY HEAD",
-        image: "./assets/images/teams-img2.png",
-        socials: {
-            facebook: "https://www.facebook.com/example2",
-            twitter: "https://twitter.com/example2",
-            instagram: "https://www.instagram.com/example2"
-        }
-    },
-    {
-        name: "Duglas Rovland",
-        role: "CYBER ANALYST",
-        image: "./assets/images/teams-img3.png",
-        socials: {
-            facebook: "https://www.facebook.com/example3",
-            twitter: "https://twitter.com/example3",
-            instagram: "https://www.instagram.com/example3"
-        }
-    },
-    {
-        name: "Christin Jonson",
-        role: "ETHICAL HACKER",
-        image: "./assets/images/teams-img4.png",
-        socials: {
-            facebook: "https://www.facebook.com/example4",
-            twitter: "https://twitter.com/example4",
-            instagram: "https://www.instagram.com/example4"
-        }
     }
 ];
 
@@ -51,13 +21,6 @@ function createTeamCard(member) {
                     <img src="${member.image}" alt="${member.name}">
                 </figure>
                 <div class="teams-img-content">
-                    <div class="hover_box_plus">
-                        <ul class="p-0 text-center">
-                            <li><a href="${member.socials.facebook}" class="text-decoration-none" target="_blank"><i class="fa-brands fa-facebook-f"></i></a></li>
-                            <li><a href="${member.socials.twitter}" class="text-decoration-none" target="_blank"><i class="fa-brands fa-twitter"></i></a></li>
-                            <li><a href="${member.socials.instagram}" class="text-decoration-none" target="_blank"><i class="fa-brands fa-instagram"></i></a></li>
-                        </ul>
-                    </div>
                     <div class="teams-text">
                         <h5>${member.name}</h5>
                         <p>${member.role}</p>
